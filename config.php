dc097d446be1313f85bb97700f89d6c5aadc2a3f
ehpomahuacre <43175343+ehpomahuacre@users.noreply.github.com>
2026-04-10 12:43:21 -0500

    Update config.php

--- config.php
+++ config.php
@@ -2,11 +2,11 @@
 
 function conexion_postgres()
 {
-$host = "maglev.proxy.rlwy.net";
-$port = "42432";
-$dbname = "moto_radar";
+$host = "trolley.proxy.rlwy.net";
+$port = "20856";
+$dbname = "railway";
 $user = "postgres";
-$password = "YZNreKIpgzjxlJtFLyjQwvWXdhmJtPzJ";
+$password = "AtWLHOHyCxASyPXeHhcZqLEEhOFOsnGS";
   try {
     $pdo = new PDO("pgsql:host=$host;port=$port;dbname=$dbname", $user, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
